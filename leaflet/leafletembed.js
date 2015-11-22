925662904fd829beb59a31bd40570b0e65c421de
Timothy Chung <timchunght@gmail.com>
2015-11-22 18:19:53 -0500

    Added working leaflet samples with NY coordinates

---
+++ leaflet/leafletembed.js
@@ -0,0 +1,18 @@
+var map;
+var ajaxRequest;
+var plotlist;
+var plotlayers=[];
+
+function initmap() {
+	// set up the map
+	map = new L.Map('map');
+
+	// create the tile layer with correct attribution
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 21, attribution: osmAttrib});		
+// 40.7353/-74.0055
+	// start the map in South-East England
+	map.setView(new L.LatLng(40.73, -74.0),9);
+	map.addLayer(osm);
+}
